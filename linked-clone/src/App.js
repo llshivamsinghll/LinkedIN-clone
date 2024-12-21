cebf3dc57b1e834a51e81050dd34a3b32c58039b
LLshivamsinghLL <shivamcool2705vns@gmail.com>
2024-12-21 23:14:15 +0530

    Initial commit

---
+++ linked-clone/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';  // Make sure this line is not commented out
+import Header from './Header'
+function App() {
+  return (
+    <div className='app'>
+   <Header/>
+    </div>
+  );
+}
+
+export default App;
